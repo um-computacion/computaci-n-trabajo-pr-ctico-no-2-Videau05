c9c83fa3856e110c13b538b3de948bce31c580ca
Nicolas Videau <nicolas@videau.nicolas>
2025-04-15 13:14:32 -0300

    Implementacion de pruebas para detector de palindromos

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -6,6 +6,11 @@ class TestPalindrome(unittest.TestCase):
         self.assertTrue(is_palindrome("madam"))
         self.assertTrue(is_palindrome("reconocer"))
         self.assertTrue(is_palindrome("level"))
+    
+    def test_phrase_palindromes(self):
+        self.assertTrue(is_palindrome("A man, a plan, a canal: Panama"))
+        self.assertTrue(is_palindrome("Adan salta y Atlas nada"))
+        self.assertTrue(is_palindrome("Sometamos o matemos"))
 
 if __name__ == '__main__':
     unittest.main()
